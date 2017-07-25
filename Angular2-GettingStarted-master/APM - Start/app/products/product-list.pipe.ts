ad19676138363234a646ec065651ebb4f94238c7
Eduardo Velez <eduardo.velez@gfrmedia.com>
2017-07-25 18:42:45 -0400

    Add new code to filter products

--- Angular2-GettingStarted-master/APM - Start/app/products/product-list.pipe.ts
+++ Angular2-GettingStarted-master/APM - Start/app/products/product-list.pipe.ts
@@ -7,9 +7,15 @@ import { IProduct } from './product'
 
 export class ProductFilterPipe implements PipeTransform {
 
-    transform(value: IProduct[], args: string[]): IProduct[]{
+    /*transform(value: IProduct[], args: string[]): IProduct[]{
        let filter: string = args[0] ? args[0].toLocaleLowerCase() : null;
        return filter ? value.filter((product: IProduct) =>
             product.productName.toLocaleLowerCase().indexOf(filter) != -1) : value;
+    }*/
+        
+    transform(value: IProduct[], filterBy: string): IProduct[] {
+        filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
+        return filterBy ? value.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1) : value;
     }
 }
