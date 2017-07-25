59485da74b84c59536dbd74cd0674e857f2faf2c
Eduardo Velez <eduardo.velez@gfrmedia.com>
2017-07-25 11:47:06 -0400

    Import star component into app.module

--- Angular2-GettingStarted-master/APM - Start/app/app.module.ts
+++ Angular2-GettingStarted-master/APM - Start/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component'
 import { FormsModule } from '@angular/forms';
-import {ProductFilterPipe} from './products/product-list.pipe'
+import { ProductFilterPipe } from './products/product-list.pipe'
+import { SharedModule } from './shared/shared.module'
+
 
 @NgModule({
-  imports: [ BrowserModule , FormsModule],
+  imports: [ BrowserModule , FormsModule, SharedModule],
   declarations: [ AppComponent, ProductListComponent, ProductFilterPipe ],
   bootstrap: [ AppComponent ]
 })
