e0a1e1520d88b094b28a31b370cb70220f8bade8
Eduardo Velez <eduardo.velez@gfrmedia.com>
2017-07-25 23:15:20 -0400

    Imported welcome and product detail component

--- Angular2-GettingStarted-master/APM - Start/app/app.module.ts
+++ Angular2-GettingStarted-master/APM - Start/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+
 import { ProductListComponent } from './products/product-list.component'
-import { FormsModule } from '@angular/forms';
 import { ProductFilterPipe } from './products/product-list.pipe'
+import { ProductDetail } from './products/product-detail.component'
+
+import { WelcomeComponent } from './home/welcome.component'
+
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module'
 import { HttpModule } from '@angular/http';
 
 
 @NgModule({
   imports: [ BrowserModule , FormsModule, SharedModule, HttpModule ],
-  declarations: [ AppComponent, ProductListComponent, ProductFilterPipe ],
+  declarations: [ AppComponent, ProductListComponent, ProductFilterPipe, ProductDetail, WelcomeComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
