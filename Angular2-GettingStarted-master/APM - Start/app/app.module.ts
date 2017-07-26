a4379f9f26333c360545387288ef475b0d66fc26
Eduardo Velez <eduardo.velez@gfrmedia.com>
2017-07-25 22:11:39 -0400

    Added HttpModule

--- Angular2-GettingStarted-master/APM - Start/app/app.module.ts
+++ Angular2-GettingStarted-master/APM - Start/app/app.module.ts
@@ -6,10 +6,11 @@ import { ProductListComponent } from './products/product-list.component'
 import { FormsModule } from '@angular/forms';
 import { ProductFilterPipe } from './products/product-list.pipe'
 import { SharedModule } from './shared/shared.module'
+import { HttpModule } from '@angular/http';
 
 
 @NgModule({
-  imports: [ BrowserModule , FormsModule, SharedModule],
+  imports: [ BrowserModule , FormsModule, SharedModule, HttpModule ],
   declarations: [ AppComponent, ProductListComponent, ProductFilterPipe ],
   bootstrap: [ AppComponent ]
 })
